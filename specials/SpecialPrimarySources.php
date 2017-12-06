99d2ba558f178201ef705e7bab4b308586619b60
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2017-12-06 18:15:49 +0100

    add web interface (special page) for data providers

--- specials/SpecialPrimarySources.php
+++ specials/SpecialPrimarySources.php
@@ -19,12 +19,51 @@ class SpecialPrimarySources extends SpecialPage {
      */
     public function execute( $sub ) {
         $out = $this->getOutput();
+        $user = $this->getUser();
 
-        $out->setPageTitle( $this->msg( 'PrimarySources-placeholder' ) );
+        $out->setPageTitle( 'Data Curation' ); 
 
-        // Parses message from .i18n.php as wikitext and adds it to the
-        // page output.
-        $out->addWikiMsg( 'PrimarySources-placeholder' );
+        if($user->isLoggedIn() || $sub == "alohomora"){
+            // Parses message from .i18n.php as wikitext and adds it to the
+            // page output.
+
+            $out->addHTML('<script>
+                            function swap(){
+                                if($("#swap").text() == "I want to update a dataset"){
+                                    $("#uploadForm").hide();
+                                    $("#updateForm").show();
+                                    $("#swap").text("I want to upload a dataset");
+                                }
+                                else{
+                                    $("#updateForm").hide();
+                                    $("#uploadForm").show();
+                                    $("#swap").text("I want to update a dataset");
+                                }
+                            }
+                            </script>');
+                            
+            $out->addHTML('<button id="swap" onClick="swap()">I want to update a dataset</button><br><br>');
+            
+            $out->addHTML('<form id="uploadForm" action="http://it.dbpedia.org/pst/upload" method="post" enctype="multipart/form-data">
+                          <label>Upload Dataset</label><br><br>
+                          <input type="hidden" name="user" value="' . $user->getName() . '">
+                          Dataset name: <input type="text" name="name" value="dataset name"><br>
+                          Dataset file: <input type="file" name="dataset" id="dataset" multiple><br><br>
+                          <input type="button" onclick="if($(\'#dataset\').get(0).files.length == 0){alert(\'Please select a file\')}else{submit()}" value="Submit">
+                          </form>');
+
+            $out->addHTML('<form id="updateForm" action="http://it.dbpedia.org/pst/update" method="post" enctype="multipart/form-data" style="display:none">
+                          <label>Update Dataset</label><br><br>
+                          <input type="hidden" name="user" value="' . $user->getName() . '">
+                          Dataset URI: <input type="text" name="dataset" value="dataset URI"><br>
+                          Dataset file to remove: <input type="file" name="remove" id="remove"><br>
+                          Dataset file to add: <input type="file" name="add" id="add"><br><br>
+                          <input type="button" onclick="if($(\'#remove\').get(0).files.length == 0 || $(\'#add\').get(0).files.length == 0 ){alert(\'Please select a file for both inputs\')}else{submit()}" value="Submit">
+                          </form>');
+        }
+        else{
+            $out->addWikiText( strtoupper('Please log in to use this feature') );
+        }
     }
 
     protected function getGroupName() {
