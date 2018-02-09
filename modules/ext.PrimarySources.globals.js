29aece5679e27455660b5d7ced388e92562d0e07
Marco Fossati <fossati@fbk.eu>
2018-02-09 17:23:51 +0100

    restructure globals module

--- modules/ext.PrimarySources.globals.js
+++ modules/ext.PrimarySources.globals.js
@@ -1,60 +1,56 @@
-(function(mw) {
+(function (mw, $) {
   console.log("Primary sources tool - globals module");
 
-    // Attach to window and globally
-    window.primarySources = window.ps = {};
-  
-  // accessible object
-  var globals = {};
+  var ps = mw.ps || {};
 
-  var API_BASE_URI = 'https://pst.wmflabs.org/pst/';
-  
-  globals.API_ENDPOINTS = {
-      DATASETS_SERVICE: API_BASE_URI + 'datasets',
-      RANDOM_SERVICE: API_BASE_URI + 'random',
-      SUGGEST_SERVICE: API_BASE_URI + 'suggest?qid={{qid}}',
-      CURATE_SERVICE: API_BASE_URI + 'curate',
-      SEARCH_SERVICE: API_BASE_URI + 'search',
-      SOURCE_URL_BLACKLIST: 'https://www.wikidata.org/w/api.php' + '?action=parse&format=json&prop=text' + '&page=Wikidata:Primary_sources_tool/URL_blacklist',
-      SOURCE_URL_WHITELIST: 'https://www.wikidata.org/w/api.php' + '?action=parse&format=json&prop=text' + '&page=Wikidata:Primary_sources_tool/URL_whitelist',
-      WIKIDATA_ENTITY_DATA_URL: 'https://www.wikidata.org/wiki/Special:EntityData/{{qid}}.json'
-  };
-
-  globals.WIKIDATA_API_COMMENT = 'Added via [[Wikidata:Primary sources tool]]';
-
-  globals.STATEMENT_STATES = {
-    unapproved: 'new',
-    approved: 'approved',
-    rejected: 'rejected',
-    duplicate: 'duplicate',
-    blacklisted: 'blacklisted'
-  };
-  globals.STATEMENT_FORMAT = 'QuickStatement';
+  // Get the selected dataset from a cookie
+  var dataset;
+  mw.loader.using(['mediawiki.cookie']).then(function () {
+    dataset = mw.cookie.get('ps-dataset', null, '');
+  });
+  var api_base_uri = 'https://pst.wmflabs.org/pst/';
 
   // Uncomment the following line to enable debug log messages
   localStorage.setItem('f2w_debug', true);
-  globals.DEBUG = JSON.parse(localStorage.getItem('f2w_debug')) || false;
-  
-  globals.FAKE_OR_RANDOM_DATA = JSON.parse(localStorage.getItem('f2w_fakeOrRandomData')) || false;
-  
-  globals.CACHE_EXPIRY = 60 * 60 * 1000;
+  // Uncomment the following line to add fake blue statements for testing
+  // localStorage.setItem('f2w_fakeOrRandomData', true);
 
-  var DATASET = null;
-  mw.loader.using(['mediawiki.cookie']).then(function() {
-      DATASET = mw.cookie.get('ps-dataset', null, '');
-  });
-  globals.DATASET = DATASET;
-  
-  // SPARQL query to get the set of statement item values in the back end
-  globals.STATEMENT_VALUES =
-  'SELECT DISTINCT ?statement_value WHERE {' +
-  '  GRAPH ?dataset {' +
-  '  ?statement_node ?statement_property ?statement_value .' +
-  '  FILTER STRSTARTS(str(?statement_node), "http://www.wikidata.org/entity/statement/") .' +
-  '  FILTER STRSTARTS(str(?statement_value), "http://www.wikidata.org/entity/Q") .' +
-  '  FILTER STRENDS(str(?dataset), "new") .}' +
-  '}';
+  // accessible object
+  ps.globals = {
+    API_ENDPOINTS: {
+      DATASETS_SERVICE: api_base_uri + 'datasets',
+      RANDOM_SERVICE: api_base_uri + 'random',
+      SUGGEST_SERVICE: api_base_uri + 'suggest?qid={{qid}}',
+      CURATE_SERVICE: api_base_uri + 'curate',
+      SEARCH_SERVICE: api_base_uri + 'search',
+      SOURCE_URL_BLACKLIST: 'https://www.wikidata.org/w/api.php' + '?action=parse&format=json&prop=text' + '&page=Wikidata:Primary_sources_tool/URL_blacklist',
+      SOURCE_URL_WHITELIST: 'https://www.wikidata.org/w/api.php' + '?action=parse&format=json&prop=text' + '&page=Wikidata:Primary_sources_tool/URL_whitelist',
+      WIKIDATA_ENTITY_DATA_URL: 'https://www.wikidata.org/wiki/Special:EntityData/{{qid}}.json'
+    },
+    WIKIDATA_API_COMMENT: 'Added via [[Wikidata:Primary sources tool]]',
+    STATEMENT_STATES: {
+      unapproved: 'new',
+      approved: 'approved',
+      rejected: 'rejected',
+      duplicate: 'duplicate',
+      blacklisted: 'blacklisted'
+    },
+    STATEMENT_FORMAT: 'QuickStatement',
+    DEBUG: JSON.parse(localStorage.getItem('f2w_debug')) || false,
+    FAKE_OR_RANDOM_DATA: JSON.parse(localStorage.getItem('f2w_fakeOrRandomData')) || false,
+    CACHE_EXPIRY: 60 * 60 * 1000,
+    DATASET: dataset,
+    // SPARQL query to get the set of statement item values in the back end
+    STATEMENT_VALUES:
+      'SELECT DISTINCT ?statement_value WHERE {' +
+      '  GRAPH ?dataset {' +
+      '  ?statement_node ?statement_property ?statement_value .' +
+      '  FILTER STRSTARTS(str(?statement_node), "http://www.wikidata.org/entity/statement/") .' +
+      '  FILTER STRSTARTS(str(?statement_value), "http://www.wikidata.org/entity/Q") .' +
+      '  FILTER STRENDS(str(?dataset), "new") .}' +
+      '}'
+  };
+
   mw.ps = {};
-  mw.ps.globals = globals;
-  
-}(mediaWiki));
+
+}(mediaWiki, jQuery));
