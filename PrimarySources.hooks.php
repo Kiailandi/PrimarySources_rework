13a63b542298711e0143f4890671e5a905f25abf
Marco Fossati <fossati@fbk.eu>
2018-05-15 15:44:14 +0200

    tabs instead of spaces

--- PrimarySources.hooks.php
+++ PrimarySources.hooks.php
@@ -31,196 +31,196 @@ use ValueFormatters\FormatterOptions;
 use ValueFormatters\ValueFormatter;
 
 class PrimarySourcesHooks {
-    /**
-     * Add welcome module to the load queue of all pages
-     */
-    public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-        /*global $wgExampleEnableWelcome;
+	/**
+	 * Add welcome module to the load queue of all pages
+	 */
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		/*global $wgExampleEnableWelcome;
 
-        if ( $wgExampleEnableWelcome ) {
-            $out->addModules( 'ext.Example.welcome.init' );
-        }*/
+		if ( $wgExampleEnableWelcome ) {
+			$out->addModules( 'ext.Example.welcome.init' );
+		}*/
 
-        $out->addModules( 'ext.PrimarySources.globals' );
-        $out->addModules( 'ext.PrimarySources.commons' );
-        $out->addModules( 'ext.PrimarySources.templates' );
-        $out->addModules( 'ext.PrimarySources.sidebar' );
-        $out->addModules( 'ext.PrimarySources.itemCuration' );
-        $out->addModules( 'ext.PrimarySources.referencePreview' );
-        $out->addModules( 'ext.PrimarySources.filter' );
+		$out->addModules( 'ext.PrimarySources.globals' );
+		$out->addModules( 'ext.PrimarySources.commons' );
+		$out->addModules( 'ext.PrimarySources.templates' );
+		$out->addModules( 'ext.PrimarySources.sidebar' );
+		$out->addModules( 'ext.PrimarySources.itemCuration' );
+		$out->addModules( 'ext.PrimarySources.referencePreview' );
+		$out->addModules( 'ext.PrimarySources.filter' );
 
-        // Always return true, indicating that parser initialization should
-        // continue normally.
-        return true;
-    }
+		// Always return true, indicating that parser initialization should
+		// continue normally.
+		return true;
+	}
 
-    /**
-     * Expose configuration variables through mw.config in javascript.
-     */
-    public static function onResourceLoaderGetConfigVars( &$vars ) {
-        /*global $wgExampleEnableWelcome, $wgExampleWelcomeColorDefault, $wgExampleWelcomeColorDays;
+	/**
+	 * Expose configuration variables through mw.config in javascript.
+	 */
+	public static function onResourceLoaderGetConfigVars( &$vars ) {
+		/*global $wgExampleEnableWelcome, $wgExampleWelcomeColorDefault, $wgExampleWelcomeColorDays;
 
-        if ( $wgExampleEnableWelcome ) {
-            $vars['wgExampleWelcomeColorDefault'] = $wgExampleWelcomeColorDefault;
-            $vars['wgExampleWelcomeColorDays'] = $wgExampleWelcomeColorDays;
-        }
-        */
-        return true;
-    }
+		if ( $wgExampleEnableWelcome ) {
+			$vars['wgExampleWelcomeColorDefault'] = $wgExampleWelcomeColorDefault;
+			$vars['wgExampleWelcomeColorDays'] = $wgExampleWelcomeColorDays;
+		}
+		*/
+		return true;
+	}
 
-    /**
-     * Register parser hooks
-     * See also http://www.mediawiki.org/wiki/Manual:Parser_functions
-     */
-    public static function onParserFirstCallInit( &$parser ) {
-        /*
-        // Add the following to a wiki page to see how it works:
-        //  <dump>test</dump>
-        //  <dump foo="bar" baz="quux">test content</dump>
-        $parser->setHook( 'dump', 'PrimarySourcesHooks::parserTagDump' );
+	/**
+	 * Register parser hooks
+	 * See also http://www.mediawiki.org/wiki/Manual:Parser_functions
+	 */
+	public static function onParserFirstCallInit( &$parser ) {
+		/*
+		// Add the following to a wiki page to see how it works:
+		//  <dump>test</dump>
+		//  <dump foo="bar" baz="quux">test content</dump>
+		$parser->setHook( 'dump', 'PrimarySourcesHooks::parserTagDump' );
 
-        // Add the following to a wiki page to see how it works:
-        //  {{#echo: hello }}
-        $parser->setFunctionHook( 'echo', 'PrimarySourcesHooks::parserFunctionEcho' );
+		// Add the following to a wiki page to see how it works:
+		//  {{#echo: hello }}
+		$parser->setFunctionHook( 'echo', 'PrimarySourcesHooks::parserFunctionEcho' );
 
-        // Add the following to a wiki page to see how it works:
-        //  {{#showme: hello | hi | there }}
-        $parser->setFunctionHook( 'showme', 'PrimarySourcesHooks::parserFunctionShowme' );
-        */
-        return true;
-    }
+		// Add the following to a wiki page to see how it works:
+		//  {{#showme: hello | hi | there }}
+		$parser->setFunctionHook( 'showme', 'PrimarySourcesHooks::parserFunctionShowme' );
+		*/
+		return true;
+	}
 
-    public static function onRegisterMagicWords( &$magicWordsIds ) {
-        // Add the following to a wiki page to see how it works:
-        //  {{MYWORD}}
-        $magicWordsIds[] = 'myword';
+	public static function onRegisterMagicWords( &$magicWordsIds ) {
+		// Add the following to a wiki page to see how it works:
+		//  {{MYWORD}}
+		$magicWordsIds[] = 'myword';
 
-        return true;
-    }
+		return true;
+	}
 
-    public static function onParserGetVariableValueSwitch( &$parser, &$cache, &$magicWordId, &$ret ) {
-        /*if ( $magicWordId === 'myword' ) {
-            // Return value and cache should match. Cache is used to save
-            // additional call when it is used multiple times on a page.
-            $ret = $cache['myword'] = self::parserGetWordMyword();
-        }*/
+	public static function onParserGetVariableValueSwitch( &$parser, &$cache, &$magicWordId, &$ret ) {
+		/*if ( $magicWordId === 'myword' ) {
+			// Return value and cache should match. Cache is used to save
+			// additional call when it is used multiple times on a page.
+			$ret = $cache['myword'] = self::parserGetWordMyword();
+		}*/
 
-        return true;
-    }
+		return true;
+	}
 
-    /**
-     * This registers our database schema update(s)
-     */
-    public static function onLoadExtensionSchemaUpdates( $updater ) {
-        /*$updater->addExtensionTable( 'example_note', __DIR__ . '/sql/add-example_note.sql' );*/
+	/**
+	 * This registers our database schema update(s)
+	 */
+	public static function onLoadExtensionSchemaUpdates( $updater ) {
+		/*$updater->addExtensionTable( 'example_note', __DIR__ . '/sql/add-example_note.sql' );*/
 
-        return true;
-    }
+		return true;
+	}
 
-    public static function onOutputPageParserOutput(OutputPage &$out){
-        $wikibaseRepo = WikibaseRepo::getDefaultInstance();
-        $formatterFactory = $wikibaseRepo->getSnakFormatterFactory();
-        $termLookup = new EntityRetrievingTermLookup( $wikibaseRepo->getEntityLookup() );
-        $labelDescriptionLookup = new LanguageLabelDescriptionLookup( $termLookup, "en" );
-        $labelFormatter = new EntityIdLabelFormatter( $labelDescriptionLookup );
-        $newProperty = new PropertyId('P2' );
-        $newSnak = new PropertyNoValueSnak( $newProperty );
-        $options = new FormatterOptions( array(
-            //TODO: fallback chain
-            ValueFormatter::OPT_LANG => "en"
-        ) );
-        $snakBreadCrumbFormatter = $formatterFactory->getSnakFormatter( SnakFormatter::FORMAT_HTML, $options);
-        $newSnakHTMLGenerator  = new SnakHtmlGenerator(
-            TemplateFactory::getDefaultInstance(),
-            $snakBreadCrumbFormatter,
-            new EscapingEntityIdFormatter( $labelFormatter, 'htmlspecialchars' ),
-            new DummyLocalizedTextProvider()
-        );
-        $newHTML = $newSnakHTMLGenerator->getSnakHtml( $newSnak );
-        $out->addHTML($newHTML);
-        return true;
-    }
+	public static function onOutputPageParserOutput(OutputPage &$out){
+		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
+		$formatterFactory = $wikibaseRepo->getSnakFormatterFactory();
+		$termLookup = new EntityRetrievingTermLookup( $wikibaseRepo->getEntityLookup() );
+		$labelDescriptionLookup = new LanguageLabelDescriptionLookup( $termLookup, "en" );
+		$labelFormatter = new EntityIdLabelFormatter( $labelDescriptionLookup );
+		$newProperty = new PropertyId('P2' );
+		$newSnak = new PropertyNoValueSnak( $newProperty );
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => "en"
+		) );
+		$snakBreadCrumbFormatter = $formatterFactory->getSnakFormatter( SnakFormatter::FORMAT_HTML, $options);
+		$newSnakHTMLGenerator  = new SnakHtmlGenerator(
+			TemplateFactory::getDefaultInstance(),
+			$snakBreadCrumbFormatter,
+			new EscapingEntityIdFormatter( $labelFormatter, 'htmlspecialchars' ),
+			new DummyLocalizedTextProvider()
+		);
+		$newHTML = $newSnakHTMLGenerator->getSnakHtml( $newSnak );
+		$out->addHTML($newHTML);
+		return true;
+	}
 
-    public static function onResourceLoaderTestModules( array &$testModules, ResourceLoader &$resourceLoader ) {
-        $boilerplate = [
-            'localBasePath' => __DIR__,
-		    'remoteExtPath' => 'PrimarySources',
+	public static function onResourceLoaderTestModules( array &$testModules, ResourceLoader &$resourceLoader ) {
+		$boilerplate = [
+			'localBasePath' => __DIR__,
+			'remoteExtPath' => 'PrimarySources',
 		];
-        
-        /*$testModules['qunit']['ext.PrimarySources.tests'] = $boilerplate + array(
-	        'dependencies' => ['ext.PrimarySources.templates'],
-	        'scripts' => ['tests/qunit/ext.PrimarySources.templates.test.js']
-        );*/
-        return true;
-    }
+		
+		/*$testModules['qunit']['ext.PrimarySources.tests'] = $boilerplate + array(
+			'dependencies' => ['ext.PrimarySources.templates'],
+			'scripts' => ['tests/qunit/ext.PrimarySources.templates.test.js']
+		);*/
+		return true;
+	}
 
-    /**
-     * Parser magic word handler for {{MYWORD}}
-     *
-     * @return string: Wikitext to be rendered in the page.
-     */
-    public static function parserGetWordMyword() {
-        /*global $wgExampleMyWord;
+	/**
+	 * Parser magic word handler for {{MYWORD}}
+	 *
+	 * @return string: Wikitext to be rendered in the page.
+	 */
+	public static function parserGetWordMyword() {
+		/*global $wgExampleMyWord;
 
-        return wfEscapeWikiText( $wgExampleMyWord );*/
-    }
+		return wfEscapeWikiText( $wgExampleMyWord );*/
+	}
 
-    /**
-     * Parser hook handler for <dump>
-     *
-     * @param string $data: The content of the tag.
-     * @param array $params: The attributes of the tag.
-     * @param Parser $parser: Parser instance available to render
-     *  wikitext into html, or parser methods.
-     * @param PPFrame $frame: Can be used to see what template
-     *  arguments ({{{1}}}) this hook was used with.
-     *
-     * @return string: HTML to insert in the page.
-     */
-    public static function parserTagDump( $data, $attribs, $parser, $frame ) {
-        $dump =  array(
-            'content' => $data,
-            'atributes' => (object)$attribs,
-        );
+	/**
+	 * Parser hook handler for <dump>
+	 *
+	 * @param string $data: The content of the tag.
+	 * @param array $params: The attributes of the tag.
+	 * @param Parser $parser: Parser instance available to render
+	 *  wikitext into html, or parser methods.
+	 * @param PPFrame $frame: Can be used to see what template
+	 *  arguments ({{{1}}}) this hook was used with.
+	 *
+	 * @return string: HTML to insert in the page.
+	 */
+	public static function parserTagDump( $data, $attribs, $parser, $frame ) {
+		$dump =  array(
+			'content' => $data,
+			'atributes' => (object)$attribs,
+		);
 
-        // Very important to escape user data with htmlspecialchars() to prevent
-        // an XSS security vulnerability.
-        $html = '<pre>Dump Tag: '
-            . htmlspecialchars( FormatJson::encode( $dump, /*prettyPrint=*/true ) )
-            . '</pre>';
+		// Very important to escape user data with htmlspecialchars() to prevent
+		// an XSS security vulnerability.
+		$html = '<pre>Dump Tag: '
+			. htmlspecialchars( FormatJson::encode( $dump, /*prettyPrint=*/true ) )
+			. '</pre>';
 
-        return $html;
-    }
+		return $html;
+	}
 
-    /**
-     * Parser function handler for {{#echo: .. }}
-     *
-     * @param Parser $parser
-     * @param string $value
-     *
-     * @return string: HTML to insert in the page.
-     */
-    public static function parserFunctionEcho( $parser, $value ) {
-        //return '<pre>Echo Function: ' . htmlspecialchars( $value ) . '</pre>';
-    }
+	/**
+	 * Parser function handler for {{#echo: .. }}
+	 *
+	 * @param Parser $parser
+	 * @param string $value
+	 *
+	 * @return string: HTML to insert in the page.
+	 */
+	public static function parserFunctionEcho( $parser, $value ) {
+		//return '<pre>Echo Function: ' . htmlspecialchars( $value ) . '</pre>';
+	}
 
-    /**
-     * Parser function handler for {{#showme: .. | .. }}
-     *
-     * @param Parser $parser
-     * @param string $arg
-     *
-     * @return string: HTML to insert in the page.
-     */
-    public static function parserFunctionShowme( $parser, $value /* arg2, arg3, */ ) {
-        //$args = array_slice( func_get_args(), 2 );
-        //$showme = array(
-        //    'value' => $value,
-        //    'arguments' => $args,
-        //);
+	/**
+	 * Parser function handler for {{#showme: .. | .. }}
+	 *
+	 * @param Parser $parser
+	 * @param string $arg
+	 *
+	 * @return string: HTML to insert in the page.
+	 */
+	public static function parserFunctionShowme( $parser, $value /* arg2, arg3, */ ) {
+		//$args = array_slice( func_get_args(), 2 );
+		//$showme = array(
+		//    'value' => $value,
+		//    'arguments' => $args,
+		//);
 
-        //return '<pre>Showme Function: '
-        //    . htmlspecialchars( FormatJson::encode( $showme, /*prettyPrint=*/true ) )
-        //    . '</pre>';
-    }
+		//return '<pre>Showme Function: '
+		//    . htmlspecialchars( FormatJson::encode( $showme, /*prettyPrint=*/true ) )
+		//    . '</pre>';
+	}
 }
--- SpecialPrimarySources.php
+++ SpecialPrimarySources.php
@@ -2,109 +2,109 @@
 
 class SpecialPrimarySources extends SpecialPage {
 
-    /**
-     * Initialize the special page.
-     */
-    public function __construct() {
-        // A special page should at least have a name.
-        // We do this by calling the parent class (the SpecialPage class)
-        // constructor method with the name as first and only parameter.
-        parent::__construct( 'PrimarySources' );
-    }
+	/**
+	 * Initialize the special page.
+	 */
+	public function __construct() {
+		// A special page should at least have a name.
+		// We do this by calling the parent class (the SpecialPage class)
+		// constructor method with the name as first and only parameter.
+		parent::__construct( 'PrimarySources' );
+	}
 
-    /**
-     * Shows the page to the user.
-     * @param string $sub: The subpage string argument (if any).
-     */
-    public function execute( $sub ) {
-        $BASE_URI = 'https://pst.wmflabs.org/pst/';
-        $DATASETS_SERVICE = $BASE_URI . 'datasets';
-        $UPLOAD_SERVICE = $BASE_URI . 'upload';
-        $UPDATE_SERVICE = $BASE_URI . 'update';
+	/**
+	 * Shows the page to the user.
+	 * @param string $sub: The subpage string argument (if any).
+	 */
+	public function execute( $sub ) {
+		$BASE_URI = 'https://pst.wmflabs.org/pst/';
+		$DATASETS_SERVICE = $BASE_URI . 'datasets';
+		$UPLOAD_SERVICE = $BASE_URI . 'upload';
+		$UPDATE_SERVICE = $BASE_URI . 'update';
 
-        $out = $this->getOutput();
-        $user = $this->getUser();
+		$out = $this->getOutput();
+		$user = $this->getUser();
 
-        $out->setPageTitle( 'Upload or update dataset' ); 
+		$out->setPageTitle( 'Upload or update dataset' ); 
 
-        if($user->isLoggedIn()){
-            $datasets = json_decode(file_get_contents($DATASETS_SERVICE));
-            $keyUser = "user";
-            $keyDataset = "dataset";
-            $userDatasets = [];
-            for($i = 0; $i < count($datasets); $i++){
-                preg_match('/User:([^\/]+)/', $datasets[$i]->$keyUser, $re);
-                if($re[1] == $user->getName()){
-                    array_push($userDatasets, $datasets[$i]->$keyDataset);
-                }
-            }
-            // Enable update only if the user has uploaded at least a dataset
-            if(count($userDatasets) > 0){
-                $out->addHTML('<script>
-                                function swap(){
-                                    if($("#swap").text() == "I want to update a dataset"){
-                                        $("#uploadForm").hide();
-                                        $("#updateForm").show();
-                                        $("#swap").text("I want to upload a dataset");
-                                    }
-                                    else{
-                                        $("#updateForm").hide();
-                                        $("#uploadForm").show();
-                                        $("#swap").text("I want to update a dataset");
-                                    }
-                                }
-                                </script>');
-                                
-                $out->addHTML('<button id="swap" onClick="swap()">I want to update a dataset</button><br /><br />');
-                
-                $updateHtml= '<form id="updateForm" action="' . $UPDATE_SERVICE . '" method="post" enctype="multipart/form-data" style="display:none">
-                    <input type="hidden" name="user" value="' . $user->getName() . '">
-                    <fieldset>
-                    <legend>Update</legend>
-                    <table><tbody>
-                    <tr class="mw-htmlform-field-"><td class="mw-label"><label for="datasetToUpdate">Dataset name to update:</label></td><td class="mw-input"><select id="datasetToUpdate" name="dataset">';
+		if($user->isLoggedIn()){
+			$datasets = json_decode(file_get_contents($DATASETS_SERVICE));
+			$keyUser = "user";
+			$keyDataset = "dataset";
+			$userDatasets = [];
+			for($i = 0; $i < count($datasets); $i++){
+				preg_match('/User:([^\/]+)/', $datasets[$i]->$keyUser, $re);
+				if($re[1] == $user->getName()){
+					array_push($userDatasets, $datasets[$i]->$keyDataset);
+				}
+			}
+			// Enable update only if the user has uploaded at least a dataset
+			if(count($userDatasets) > 0){
+				$out->addHTML('<script>
+								function swap(){
+									if($("#swap").text() == "I want to update a dataset"){
+										$("#uploadForm").hide();
+										$("#updateForm").show();
+										$("#swap").text("I want to upload a dataset");
+									}
+									else{
+										$("#updateForm").hide();
+										$("#uploadForm").show();
+										$("#swap").text("I want to update a dataset");
+									}
+								}
+								</script>');
+								
+				$out->addHTML('<button id="swap" onClick="swap()">I want to update a dataset</button><br /><br />');
+				
+				$updateHtml= '<form id="updateForm" action="' . $UPDATE_SERVICE . '" method="post" enctype="multipart/form-data" style="display:none">
+					<input type="hidden" name="user" value="' . $user->getName() . '">
+					<fieldset>
+					<legend>Update</legend>
+					<table><tbody>
+					<tr class="mw-htmlform-field-"><td class="mw-label"><label for="datasetToUpdate">Dataset name to update:</label></td><td class="mw-input"><select id="datasetToUpdate" name="dataset">';
 
-                for($i = 0; $i < count($userDatasets); $i++){
-                    $updateHtml.= '<option value="' . $userDatasets[$i] . '">' . explode('/', $userDatasets[$i])[2] . '</option>';
-                }
+				for($i = 0; $i < count($userDatasets); $i++){
+					$updateHtml.= '<option value="' . $userDatasets[$i] . '">' . explode('/', $userDatasets[$i])[2] . '</option>';
+				}
 
-                $updateHtml.='</select></td></tr>
-                    <tr class="mw-htmlform-field-UpdateSourceField"><td class="mw-label"><label for="datasetToRemove">Dataset file to remove:</label></td><td class="mw-input"><input id="datasetToRemove" name="remove" type="file"></td></tr>
-                    <tr class="mw-htmlform-field-UpdateSourceField"><td class="mw-label"><label for="datasetToAdd">Dataset file to add:</label></td><td class="mw-input"><input id="datasetToAdd" name="add" type="file"></td></tr>
-                    <tr><td colspan="2" class="htmlform-tip">Maximum file size: 250 MB</td></tr>
-                    <tr><td colspan="2" class="htmlform-tip">File format allowed: RDF</td></tr>
-                    </tbody></table>
-                    </fieldset>
-                    <span class="mw-htmlform-submit-buttons">
-                    <input type="button" onclick="if($(\'#datasetToRemove\').get(0).files.length === 0 || $(\'#datasetToAdd\').get(0).files.length === 0 ){alert(\'Please select a file for both inputs\')}else{submit()}" title="Update your dataset" value="Submit">
-                    </span>
-                    </form>';
-                $out->addHTML($updateHtml);
-            }
+				$updateHtml.='</select></td></tr>
+					<tr class="mw-htmlform-field-UpdateSourceField"><td class="mw-label"><label for="datasetToRemove">Dataset file to remove:</label></td><td class="mw-input"><input id="datasetToRemove" name="remove" type="file"></td></tr>
+					<tr class="mw-htmlform-field-UpdateSourceField"><td class="mw-label"><label for="datasetToAdd">Dataset file to add:</label></td><td class="mw-input"><input id="datasetToAdd" name="add" type="file"></td></tr>
+					<tr><td colspan="2" class="htmlform-tip">Maximum file size: 250 MB</td></tr>
+					<tr><td colspan="2" class="htmlform-tip">File format allowed: RDF</td></tr>
+					</tbody></table>
+					</fieldset>
+					<span class="mw-htmlform-submit-buttons">
+					<input type="button" onclick="if($(\'#datasetToRemove\').get(0).files.length === 0 || $(\'#datasetToAdd\').get(0).files.length === 0 ){alert(\'Please select a file for both inputs\')}else{submit()}" title="Update your dataset" value="Submit">
+					</span>
+					</form>';
+				$out->addHTML($updateHtml);
+			}
 
-            $out->addHTML('<form id="uploadForm" action="' . $UPLOAD_SERVICE . '" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="user" value="' . $user->getName() . '">
-                <fieldset>
-                <legend>Upload</legend>
-                <table><tbody>
-                <tr class="mw-htmlform-field-HTMLTextField"><td class="mw-label"><label for="datasetName">Dataset name:</label></td><td class="mw-input"><input id="datasetName" type="text" name="name" value=""></td></tr>
-                <tr class="mw-htmlform-field-UploadSourceField"><td class="mw-label"><label for="datasetFiles">Dataset files:</label></td><td class="mw-input"><input id="datasetFiles" type="file" name="dataset" multiple></td></tr>
-                <tr><td colspan="2" class="htmlform-tip">Maximum file size: 250 MB</td></tr>
-                <tr><td colspan="2" class="htmlform-tip">File format allowed: RDF</td></tr>
-                </tbody></table>
-                </fieldset>
-                <span class="mw-htmlform-submit-buttons">
-                <input type="button" onclick="if($(\'#datasetFiles\').get(0).files.length === 0){alert(\'Please select a file\')}else{submit()}" title="Upload your dataset" value="Submit">
-                </span>
-                </form>');
+			$out->addHTML('<form id="uploadForm" action="' . $UPLOAD_SERVICE . '" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="user" value="' . $user->getName() . '">
+				<fieldset>
+				<legend>Upload</legend>
+				<table><tbody>
+				<tr class="mw-htmlform-field-HTMLTextField"><td class="mw-label"><label for="datasetName">Dataset name:</label></td><td class="mw-input"><input id="datasetName" type="text" name="name" value=""></td></tr>
+				<tr class="mw-htmlform-field-UploadSourceField"><td class="mw-label"><label for="datasetFiles">Dataset files:</label></td><td class="mw-input"><input id="datasetFiles" type="file" name="dataset" multiple></td></tr>
+				<tr><td colspan="2" class="htmlform-tip">Maximum file size: 250 MB</td></tr>
+				<tr><td colspan="2" class="htmlform-tip">File format allowed: RDF</td></tr>
+				</tbody></table>
+				</fieldset>
+				<span class="mw-htmlform-submit-buttons">
+				<input type="button" onclick="if($(\'#datasetFiles\').get(0).files.length === 0){alert(\'Please select a file\')}else{submit()}" title="Upload your dataset" value="Submit">
+				</span>
+				</form>');
 
-        }
-        else{
-            $out->addWikiText( strtoupper('please log in to use this feature') );
-        }
-    }
+		}
+		else{
+			$out->addWikiText( strtoupper('please log in to use this feature') );
+		}
+	}
 
-    protected function getGroupName() {
-        return 'other';
-    }
+	protected function getGroupName() {
+		return 'other';
+	}
 }
