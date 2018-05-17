6d590ba265b931c832a63d7e53675d46ee49cdb3
Marco Fossati <fossati@fbk.eu>
2018-05-17 17:54:17 +0200

    move blacklist, whitelist, entity data URLs to wikidata.org for production use

--- modules/ext.PrimarySources.globals.js
+++ modules/ext.PrimarySources.globals.js
@@ -19,9 +19,9 @@
 			SPARQL_SERVICE: BASE_URI + 'sparql',
 			PREVIEW_SERVICE: 'https://tools.wmflabs.org/strephit/search?url='
 		},
-		SOURCE_URL_BLACKLIST: 'https://wikidata-pst.wmflabs.org/w/api.php?action=parse&format=json&prop=text&page=Testwiki:Primary_sources_tool/URL_blacklist',
-		SOURCE_URL_WHITELIST: 'https://wikidata-pst.wmflabs.org/w/api.php?action=parse&format=json&prop=text&page=Testwiki:Primary_sources_tool/URL_whitelist',
-		WIKIDATA_ENTITY_DATA_URL: 'https://wikidata-pst.wmflabs.org/wiki/Special:EntityData/{{qid}}.json',
+		SOURCE_URL_BLACKLIST: 'https://www.wikidata.org/w/api.php?action=parse&format=json&prop=text&page=Wikidata:Primary_sources_tool/URL_blacklist',
+		SOURCE_URL_WHITELIST: 'https://www.wikidata.org/w/api.php?action=parse&format=json&prop=text&page=Wikidata:Primary_sources_tool/URL_whitelist',
+		WIKIDATA_ENTITY_DATA_URL: 'https://www.wikidata.org/wiki/Special:EntityData/{{qid}}.json',
 		WIKIDATA_API_COMMENT: 'Added via [[Wikidata:Primary sources tool]]',
 		STATEMENT_STATES: {
 			'new': 'new',
